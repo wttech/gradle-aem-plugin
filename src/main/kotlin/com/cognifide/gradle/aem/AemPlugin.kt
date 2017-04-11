2536e0b8f22e2047f8bab443623957ba25d179c0
krystian.panek <krystian.panek@cognifide.com>
2017-04-11 09:41:46 +0200

    Kotlin STDLIB added

--- src/main/kotlin/com/cognifide/gradle/aem/AemPlugin.kt
+++ src/main/kotlin/com/cognifide/gradle/aem/AemPlugin.kt
@@ -2,11 +2,12 @@ package com.cognifide.gradle.aem
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.plugins.BasePlugin
 
 class AemPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
-        // initially empty
+        project.plugins.apply(BasePlugin::class.java)
     }
 
 }
