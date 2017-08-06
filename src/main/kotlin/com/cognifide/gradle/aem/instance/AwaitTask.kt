dce6bec87bc54005fbc89b9145561a01e962f0da
punky <punky@neva.zone>
2017-08-06 20:29:39 +0200

    Await extracted

--- src/main/kotlin/com/cognifide/gradle/aem/instance/AwaitTask.kt
+++ src/main/kotlin/com/cognifide/gradle/aem/instance/AwaitTask.kt
@@ -2,8 +2,6 @@ package com.cognifide.gradle.aem.instance
 
 import com.cognifide.gradle.aem.AemTask
 import com.cognifide.gradle.aem.deploy.SyncTask
-import com.cognifide.gradle.aem.internal.Behaviors
-import com.cognifide.gradle.aem.internal.ProgressLogger
 import org.gradle.api.tasks.TaskAction
 
 open class AwaitTask : SyncTask() {
@@ -15,51 +13,7 @@ open class AwaitTask : SyncTask() {
 
     @TaskAction
     fun await() {
-        waitUntilStableInstances(Instance.locals(project))
-    }
-
-    private fun waitUntilStableInstances(instances: List<Instance>) {
-        if (config.instanceAwaitDelay > 0) {
-            logger.info("Delaying instance stability checking")
-            Behaviors.waitFor(config.instanceAwaitDelay)
-        }
-
-        val progressLogger = ProgressLogger(project, "Awaiting stable instance(s)")
-
-        progressLogger.started()
-
-        Behaviors.waitUntil(config.instanceAwaitInterval, { elapsed, attempt ->
-            if (config.instanceAwaitTimeout > 0 && elapsed > config.instanceAwaitTimeout) {
-                logger.warn("Timeout reached while awaiting stable instance(s)")
-                return@waitUntil false
-            }
-
-            val instanceStates = instances.map { InstanceState(project, it, config.instanceAwaitTimeout) }
-            val instanceProgress = instanceStates.map { instanceStateMessage(it, attempt) }.joinToString(" | ")
-
-            progressLogger.progress(instanceProgress)
-
-            if (instanceStates.all { it.stable }) {
-                logger.info("Instance(s) are now stable.")
-                return@waitUntil false
-            }
-
-            true
-        })
-
-        progressLogger.completed()
-    }
-
-    private fun instanceStateMessage(it: InstanceState, attempt: Long): String {
-        return "${it.instance.name}: ${instanceProgressIndicator(it, attempt)} ${it.bundleState.stats} [${it.bundleState.stablePercent}]"
-    }
-
-    private fun instanceProgressIndicator(state: InstanceState, attempt: Long): String {
-        return if (state.stable || attempt.rem(2) == 0L) {
-            "*"
-        } else {
-            " "
-        }
+        InstanceState.awaitStable(project)
     }
 
 }
--- src/main/kotlin/com/cognifide/gradle/aem/instance/InstanceState.kt
+++ src/main/kotlin/com/cognifide/gradle/aem/instance/InstanceState.kt
@@ -2,11 +2,62 @@ package com.cognifide.gradle.aem.instance
 
 import com.cognifide.gradle.aem.AemConfig
 import com.cognifide.gradle.aem.deploy.DeploySynchronizer
+import com.cognifide.gradle.aem.internal.Behaviors
+import com.cognifide.gradle.aem.internal.ProgressLogger
 import org.apache.commons.httpclient.params.HttpConnectionParams
 import org.gradle.api.Project
 import org.gradle.api.logging.Logger
 
 class InstanceState(val project: Project, val instance: Instance, val timeout: Int) {
+
+    companion object {
+        fun awaitStable(project: Project, instances: List<Instance> = Instance.locals(project)) {
+            val config = AemConfig.of(project)
+            val logger = project.logger
+
+            if (config.instanceAwaitDelay > 0) {
+                logger.info("Delaying instance stability checking")
+                Behaviors.waitFor(config.instanceAwaitDelay)
+            }
+
+            val progressLogger = ProgressLogger(project, "Awaiting stable instance(s)")
+
+            progressLogger.started()
+
+            Behaviors.waitUntil(config.instanceAwaitInterval, { elapsed, attempt ->
+                if (config.instanceAwaitTimeout > 0 && elapsed > config.instanceAwaitTimeout) {
+                    logger.warn("Timeout reached while awaiting stable instance(s)")
+                    return@waitUntil false
+                }
+
+                val instanceStates = instances.map { InstanceState(project, it, config.instanceAwaitTimeout) }
+                val instanceProgress = instanceStates.map { progressFor(it, attempt) }.joinToString(" | ")
+
+                progressLogger.progress(instanceProgress)
+
+                if (instanceStates.all { it.stable }) {
+                    logger.info("Instance(s) are now stable.")
+                    return@waitUntil false
+                }
+
+                true
+            })
+
+            progressLogger.completed()
+        }
+
+        private fun progressFor(it: InstanceState, attempt: Long): String {
+            return "${it.instance.name}: ${progressIndicator(it, attempt)} ${it.bundleState.stats} [${it.bundleState.stablePercent}]"
+        }
+
+        private fun progressIndicator(state: InstanceState, attempt: Long): String {
+            return if (state.stable || attempt.rem(2) == 0L) {
+                "*"
+            } else {
+                " "
+            }
+        }
+    }
 
     val logger: Logger = project.logger
 
@@ -14,7 +65,7 @@ class InstanceState(val project: Project, val instance: Instance, val timeout: I
 
     val sync = DeploySynchronizer(instance, config)
 
-    private var syncParametrizer: (HttpConnectionParams) -> Unit = { params ->
+    var syncParametrizer: (HttpConnectionParams) -> Unit = { params ->
         params.connectionTimeout = timeout
         params.soTimeout = timeout
     }
