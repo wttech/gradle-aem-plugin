961136820b131ce66af314978aac388b794080a5
krystian.panek <krystian.panek@cognifide.com>
2018-05-03 09:09:14 +0200

    Jar names defaults

--- src/test/kotlin/com/cognifide/gradle/aem/test/ComposeTaskTest.kt
+++ src/test/kotlin/com/cognifide/gradle/aem/test/ComposeTaskTest.kt
@@ -11,7 +11,7 @@ class ComposeTaskTest : AemTest() {
 
             assertPackage(pkg)
             assertPackageFile(pkg, "jcr_root/apps/example/.content.xml")
-            assertPackageFile(pkg, "jcr_root/apps/example/install/example-1.0.0-SNAPSHOT.jar")
+            assertPackageFile(pkg, "jcr_root/apps/example/install/com.company.aem.example-1.0.0-SNAPSHOT.jar")
         })
     }
 
@@ -21,21 +21,21 @@ class ComposeTaskTest : AemTest() {
             val assemblyPkg = file("build/distributions/example-1.0.0-SNAPSHOT.zip")
             assertPackage(assemblyPkg)
             assertPackageFile(assemblyPkg, "jcr_root/apps/example/core/.content.xml")
-            assertPackageFile(assemblyPkg, "jcr_root/apps/example/core/install/core-1.0.0-SNAPSHOT.jar")
+            assertPackageFile(assemblyPkg, "jcr_root/apps/example/core/install/com.company.aem.core-1.0.0-SNAPSHOT.jar")
             assertPackageFile(assemblyPkg, "jcr_root/apps/example/common/.content.xml")
-            assertPackageFile(assemblyPkg, "jcr_root/apps/example/common/install/common-1.0.0-SNAPSHOT.jar")
+            assertPackageFile(assemblyPkg, "jcr_root/apps/example/common/install/com.company.aem.common-1.0.0-SNAPSHOT.jar")
             assertPackageFile(assemblyPkg, "jcr_root/apps/example/common/install/kotlin-osgi-bundle-1.2.21.jar")
             assertPackageFile(assemblyPkg, "jcr_root/etc/designs/example/.content.xml")
 
             val corePkg = file("core/build/distributions/example-core-1.0.0-SNAPSHOT.zip")
             assertPackage(corePkg)
             assertPackageFile(corePkg, "jcr_root/apps/example/core/.content.xml")
-            assertPackageFile(corePkg, "jcr_root/apps/example/core/install/core-1.0.0-SNAPSHOT.jar")
+            assertPackageFile(corePkg, "jcr_root/apps/example/core/install/com.company.aem.core-1.0.0-SNAPSHOT.jar")
 
             val commonPkg = file("common/build/distributions/example-common-1.0.0-SNAPSHOT.zip")
             assertPackage(commonPkg)
             assertPackageFile(commonPkg, "jcr_root/apps/example/common/.content.xml")
-            assertPackageFile(commonPkg, "jcr_root/apps/example/common/install/common-1.0.0-SNAPSHOT.jar")
+            assertPackageFile(commonPkg, "jcr_root/apps/example/common/install/com.company.aem.common-1.0.0-SNAPSHOT.jar")
             assertPackageFile(commonPkg, "jcr_root/apps/example/common/install/kotlin-osgi-bundle-1.2.21.jar")
 
             val designPkg = file("design/build/distributions/example-design-1.0.0-SNAPSHOT.zip")
