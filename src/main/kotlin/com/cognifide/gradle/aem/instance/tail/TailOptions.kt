a855830b63077197198586664dd35e587696d7fd
mierzwid <mierzwid@gmail.com>
2019-01-25 13:47:17 +0100

    #138 logs tailing - fixing import ordering ;)

--- src/main/kotlin/com/cognifide/gradle/aem/instance/tail/TailOptions.kt
+++ src/main/kotlin/com/cognifide/gradle/aem/instance/tail/TailOptions.kt
@@ -2,10 +2,10 @@ package com.cognifide.gradle.aem.instance.tail
 
 import com.cognifide.gradle.aem.common.AemExtension
 import com.cognifide.gradle.aem.common.Patterns
-import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.Internal
 import java.nio.file.Paths
 import kotlin.math.max
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Internal
 
 class TailOptions(aem: AemExtension) {
 
