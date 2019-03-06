4c08f3a263e1fc3d536b9524dd770143331c4e7c
krystian.panek <krystian.panek@cognifide.com>
2019-03-06 09:37:28 +0100

    Version bump

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,7 +10,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.21")
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.0.0-RC14")
     implementation("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
-    implementation("com.neva.gradle:fork-plugin:3.0.6")
+    implementation("com.neva.gradle:fork-plugin:3.1.1")
     implementation("net.researchgate:gradle-release:2.6.0")
 
 }
