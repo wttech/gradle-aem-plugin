b569d02f15db7691d57cc36787f423ebbc3066dc
krystian.panek <krystian.panek@cognifide.com>
2019-03-11 08:24:41 +0100

    Minor API improvement

--- src/main/kotlin/com/cognifide/gradle/aem/common/AemExtension.kt
+++ src/main/kotlin/com/cognifide/gradle/aem/common/AemExtension.kt
@@ -246,7 +246,7 @@ open class AemExtension(@Internal val project: Project) {
 
     fun sync(synchronizer: InstanceSync.() -> Unit) = sync(instances, synchronizer)
 
-    fun sync(instance: Instance, synchronizer: InstanceSync.() -> Unit) = instance.sync(synchronizer)
+    fun <T> sync(instance: Instance, synchronizer: InstanceSync.() -> T) = instance.sync(synchronizer)
 
     fun sync(instances: Collection<Instance>, synchronizer: InstanceSync.() -> Unit) {
         parallel.with(instances) { this.sync.apply(synchronizer) }
