9f669c7192d9139f385cd0b45c222911a78f2648
krystian.panek <krystian.panek@cognifide.com>
2019-06-03 16:12:54 +0200

    Progress indicator fix

--- src/main/kotlin/com/cognifide/gradle/aem/common/build/ProgressIndicator.kt
+++ src/main/kotlin/com/cognifide/gradle/aem/common/build/ProgressIndicator.kt
@@ -38,7 +38,7 @@ class ProgressIndicator(private val project: Project) {
     private fun <T> ProgressIndicator.launchAsync(block: ProgressIndicator.() -> T): T {
         return runBlocking {
             val blockJob = async(Dispatchers.Default) { block() }
-            val loggerJob = async(Dispatchers.Default) {
+            val loggerJob = async(Dispatchers.IO) {
                 ProgressLogger.of(project).launch {
                     this@ProgressIndicator.logger = this
                     Behaviors.waitUntil(delay) { timer ->
