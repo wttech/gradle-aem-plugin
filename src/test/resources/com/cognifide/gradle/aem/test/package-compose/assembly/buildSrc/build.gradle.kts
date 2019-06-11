6b7188eafa05e64e0f5793ea5c948866313c85f2
krystian.panek <krystian.panek@cognifide.com>
2019-06-11 14:45:25 +0200

    Release 7.0.0-beta2

--- src/test/resources/com/cognifide/gradle/aem/test/package-compose/assembly/buildSrc/build.gradle.kts
+++ src/test/resources/com/cognifide/gradle/aem/test/package-compose/assembly/buildSrc/build.gradle.kts
@@ -4,6 +4,6 @@ repositories {
 }
 
 dependencies {
-    implementation("com.cognifide.gradle:aem-plugin:7.0.0-beta")
+    implementation("com.cognifide.gradle:aem-plugin:7.0.0-beta2")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.30")
 }
