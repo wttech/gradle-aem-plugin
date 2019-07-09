c03d2ac0c19ba5a1c60186391e259bce104bd1fd
krystian.panek <krystian.panek@cognifide.com>
2019-07-09 12:17:45 +0200

    Instance init HTTP client case refactored

--- src/main/kotlin/com/cognifide/gradle/aem/common/instance/InstanceHttpClient.kt
+++ src/main/kotlin/com/cognifide/gradle/aem/common/instance/InstanceHttpClient.kt
@@ -1,9 +1,11 @@
 package com.cognifide.gradle.aem.common.instance
 
 import com.cognifide.gradle.aem.AemExtension
+import com.cognifide.gradle.aem.common.build.BuildScope
 import com.cognifide.gradle.aem.common.http.HttpClient
 import com.cognifide.gradle.aem.common.http.ResponseException
 import org.apache.http.HttpResponse
+import org.apache.http.HttpStatus
 
 open class InstanceHttpClient(aem: AemExtension, val instance: Instance) : HttpClient(aem) {
 
@@ -12,6 +14,28 @@ open class InstanceHttpClient(aem: AemExtension, val instance: Instance) : HttpC
         basicUser = instance.user
         basicPassword = instance.password
         authorizationPreemptive = true
+
+        if (instance.run { this is LocalInstance && !initialized }) {
+            val buildScope = BuildScope.of(aem.project)
+            val authInitKey = "${instance.name}.authInit"
+            val authInit = buildScope.get(authInitKey) ?: false
+
+            if (authInit) {
+                basicUser = Instance.USER_DEFAULT
+                basicPassword = Instance.PASSWORD_DEFAULT
+            }
+
+            responseHandler = { response ->
+                if (response.statusLine.statusCode == HttpStatus.SC_UNAUTHORIZED) {
+                    if (authInit) {
+                        aem.logger.info("Switching instance credentials from customized to defaults.")
+                    } else {
+                        aem.logger.info("Switching instance credentials from defaults to customized.")
+                    }
+                    buildScope.put(authInitKey, !authInit)
+                }
+            }
+        }
 
         apply { aem.instanceOptions.httpOptions(this, instance) }
     }
--- src/main/kotlin/com/cognifide/gradle/aem/common/instance/check/DefaultCheck.kt
+++ src/main/kotlin/com/cognifide/gradle/aem/common/instance/check/DefaultCheck.kt
@@ -1,10 +1,6 @@
 package com.cognifide.gradle.aem.common.instance.check
 
-import com.cognifide.gradle.aem.common.build.BuildScope
-import com.cognifide.gradle.aem.common.instance.Instance
 import com.cognifide.gradle.aem.common.instance.InstanceSync
-import com.cognifide.gradle.aem.common.instance.LocalInstance
-import org.apache.http.HttpStatus
 import org.gradle.api.logging.LogLevel
 
 abstract class DefaultCheck(protected val group: CheckGroup) : Check {
@@ -21,30 +17,8 @@ abstract class DefaultCheck(protected val group: CheckGroup) : Check {
     val statusLogger = group.statusLogger
 
     var sync: InstanceSync = instance.sync.apply {
-        val init = instance.run { this is LocalInstance && !initialized }
-
-        val scope = BuildScope.of(aem.project)
-        val authInitKey = "${instance.name}.authInit"
-        val authInit = scope.get(authInitKey) ?: false
-
         http.connectionTimeout = 1000
         http.connectionRetries = false
-
-        if (init && authInit) {
-            http.basicUser = Instance.USER_DEFAULT
-            http.basicPassword = Instance.PASSWORD_DEFAULT
-        }
-
-        http.responseHandler = { response ->
-            if (init && response.statusLine.statusCode == HttpStatus.SC_UNAUTHORIZED) {
-                if (authInit) {
-                    aem.logger.info("Switching instance credentials from customized to defaults.")
-                } else {
-                    aem.logger.info("Switching instance credentials from defaults to customized.")
-                }
-                scope.put(authInitKey, !authInit)
-            }
-        }
     }
 
     override val status: String
