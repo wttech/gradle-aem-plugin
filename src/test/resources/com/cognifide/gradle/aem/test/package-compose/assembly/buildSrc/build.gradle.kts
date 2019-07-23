0c6aacd3ccb68d2909d2f7f7cfb63a4b969fa85c
krystian.panek <krystian.panek@cognifide.com>
2019-07-23 10:02:52 +0200

    Version bump

--- src/test/resources/com/cognifide/gradle/aem/test/package-compose/assembly/buildSrc/build.gradle.kts
+++ src/test/resources/com/cognifide/gradle/aem/test/package-compose/assembly/buildSrc/build.gradle.kts
@@ -4,6 +4,6 @@ repositories {
 }
 
 dependencies {
-    implementation("com.cognifide.gradle:aem-plugin:7.0.7")
+    implementation("com.cognifide.gradle:aem-plugin:7.0.8")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.30")
 }
