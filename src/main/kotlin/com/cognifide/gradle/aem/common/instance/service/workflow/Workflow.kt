11142d32ab5b8cb94561fb83569d17488259ffb9
krystian.panek <krystian.panek@cognifide.com>
2019-08-29 09:54:07 +0200

    Workflow logging

--- src/main/kotlin/com/cognifide/gradle/aem/common/instance/service/workflow/Workflow.kt
+++ src/main/kotlin/com/cognifide/gradle/aem/common/instance/service/workflow/Workflow.kt
@@ -7,6 +7,10 @@ class Workflow(val manager: WorkflowManager, val id: String) {
 
     private val repository = manager.repository
 
+    private val instance = manager.instance
+
+    private val logger = manager.aem.logger
+
     private val launcherNode: Node
         get() = repository.node(when {
             manager.configFrozen -> "/conf/global/settings/workflow/launcher/config/$id"
@@ -24,7 +28,14 @@ class Workflow(val manager: WorkflowManager, val id: String) {
 
     fun toggle(flag: Boolean) {
         if (manager.configFrozen && !launcherNode.exists) {
+            logger.info("Copying workflow launcher from '${launcherFrozenNode.path}' to ${launcherNode.path} on $instance")
             launcherNode.copyFrom(launcherFrozenNode.path)
+        }
+
+        if (flag) {
+            logger.info("Enabling workflow launcher '${launcherNode.path}' on $instance")
+        } else {
+            logger.info("Disabling workflow launcher '${launcherNode.path}' on $instance")
         }
 
         launcherNode.saveProperty("enabled", flag)
