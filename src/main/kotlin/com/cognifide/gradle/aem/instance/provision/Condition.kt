43e5382d19a30c1a16bb5f047c78820db894a7a6
krystian.panek <krystian.panek@cognifide.com>
2019-09-12 12:13:23 +0200

    Refactorings

--- src/main/kotlin/com/cognifide/gradle/aem/instance/provision/Condition.kt
+++ src/main/kotlin/com/cognifide/gradle/aem/instance/provision/Condition.kt
@@ -1,9 +1,6 @@
 package com.cognifide.gradle.aem.instance.provision
 
 import com.cognifide.gradle.aem.common.utils.Formats
-import java.time.LocalDateTime
-import java.time.ZoneId
-import java.time.temporal.ChronoUnit
 import java.util.concurrent.TimeUnit
 
 class Condition(val step: InstanceStep) {
@@ -12,17 +9,7 @@ class Condition(val step: InstanceStep) {
 
     fun once(): Boolean = !step.done
 
-    fun afterMillis(millis: Long): Boolean {
-        if (!step.done) {
-            return true
-        }
-
-        val nowTimestamp = LocalDateTime.now().atZone(ZoneId.systemDefault())
-        val thenTimestamp = Formats.dateTime(step.endedAt.time, step.instance.zoneId)
-        val diffMillis = ChronoUnit.MILLIS.between(thenTimestamp, nowTimestamp)
-
-        return diffMillis < millis
-    }
+    fun afterMillis(millis: Long): Boolean = once() || Formats.timeUp(step.endedAt.time, instance.zoneId, millis)
 
     fun afterDays(count: Long) = afterMillis(TimeUnit.DAYS.toMillis(count))
 
