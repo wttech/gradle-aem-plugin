2ef4620f4b60805693bdcf19fe4208c352e5024e
krystian.panek <krystian.panek@cognifide.com>
2019-10-17 11:13:33 +0200

    Bump

---
+++ src/main/kotlin/com/cognifide/gradle/aem/common/instance/check/CheckFactory.kt
@@ -0,0 +1,18 @@
+package com.cognifide.gradle.aem.common.instance.check
+
+class CheckFactory(val group: CheckGroup) {
+
+    fun custom(callback: CustomCheck.() -> Unit) = CustomCheck(group, callback)
+
+    fun bundles(options: BundlesCheck.() -> Unit) = BundlesCheck(group).apply(options)
+
+    fun components(options: ComponentsCheck.() -> Unit) = ComponentsCheck(group).apply(options)
+
+    fun events(options: EventsCheck.() -> Unit) = EventsCheck(group).apply(options)
+
+    fun timeout(options: TimeoutCheck.() -> Unit) = TimeoutCheck(group).apply(options)
+
+    fun unavailable(options: UnavailableCheck.() -> Unit) = UnavailableCheck(group).apply(options)
+
+    fun unchanged(options: UnchangedCheck.() -> Unit) = UnchangedCheck(group).apply(options)
+}
