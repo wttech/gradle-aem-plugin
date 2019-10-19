12e5694b8b814bcdf7af6f8980ef41721b496d3a
krystian.panek <krystian.panek@cognifide.com>
2019-10-19 09:43:42 +0200

    Github release plugin added

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ plugins {
     id("com.jfrog.bintray")
     id("com.neva.fork")
     id("net.researchgate.release")
+    id("com.github.breadmoirai.github-release")
 }
 
 group = "com.cognifide.gradle"
@@ -132,7 +133,7 @@ tasks {
     }
 
     named("afterReleaseBuild") {
-        dependsOn("bintrayUpload", "publishPlugins")
+        dependsOn("bintrayUpload", "publishPlugins", "githubRelease")
     }
 
     named("updateVersion") {
@@ -147,5 +148,114 @@ detekt {
     failFast = true
 }
 
-apply(from = "gradle/publish.gradle.kts")
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+        }
+    }
+}
+
+gradlePlugin {
+    plugins {
+        create("common") {
+            id = "com.cognifide.aem.common"
+            implementationClass = "com.cognifide.gradle.aem.common.CommonPlugin"
+            displayName = "AEM Common Plugin"
+            description = "Provides AEM DSL / 'aem' extension to build script on which all other logic is based."
+        }
+        create("tooling") {
+            id = "com.cognifide.aem.tooling"
+            implementationClass = "com.cognifide.gradle.aem.tooling.ToolingPlugin"
+            displayName = "AEM Tooling Plugin"
+            description = "Provides tasks like 'rcp', 'sync', 'vlt' for working with content using JCR File Vault."
+        }
+        create("package") {
+            id = "com.cognifide.aem.package"
+            implementationClass = "com.cognifide.gradle.aem.pkg.PackagePlugin"
+            displayName = "AEM Package Plugin"
+            description = "Provides tasks for working with CRX packages."
+        }
+        create("bundle") {
+            id = "com.cognifide.aem.bundle"
+            implementationClass = "com.cognifide.gradle.aem.bundle.BundlePlugin"
+            displayName = "AEM Bundle Plugin"
+            description = "Adds support for building OSGi bundles."
+        }
+        create("instance") {
+            id = "com.cognifide.aem.instance"
+            implementationClass = "com.cognifide.gradle.aem.instance.InstancePlugin"
+            displayName = "AEM Instance Plugin"
+            description = "Provides tasks for working with native local AEM instances."
+        }
+        create("environment") {
+            id = "com.cognifide.aem.environment"
+            implementationClass = "com.cognifide.gradle.aem.environment.EnvironmentPlugin"
+            displayName = "AEM Environment Plugin"
+            description = "Provides tasks for working with virtualized AEM environment."
+        }
+    }
+}
+
+pluginBundle {
+    website = "https://github.com/Cognifide/gradle-aem-plugin"
+    vcsUrl = "https://github.com/Cognifide/gradle-aem-plugin.git"
+    description = "Gradle AEM Plugin"
+    tags = listOf("aem", "cq", "vault", "scr")
+}
+
+bintray {
+    user = (project.findProperty("bintray.user") ?: System.getenv("BINTRAY_USER"))?.toString()
+    key = (project.findProperty("bintray.key") ?: System.getenv("BINTRAY_KEY"))?.toString()
+    setPublications("mavenJava")
+    with(pkg) {
+        repo = "maven-public"
+        name = "gradle-aem-plugin"
+        userOrg = "cognifide"
+        setLicenses("Apache-2.0")
+        vcsUrl = "https://github.com/Cognifide/gradle-aem-plugin.git"
+        setLabels("aem", "cq", "vault", "scr")
+        with(version) {
+            name = project.version.toString()
+            desc = "${project.description} ${project.version}"
+            vcsTag = project.version.toString()
+        }
+    }
+    publish = (project.findProperty("bintray.publish") ?: "true").toString().toBoolean()
+    override = (project.findProperty("bintray.override") ?: "false").toString().toBoolean()
+}
+
+githubRelease {
+    owner("Cognifide")
+    repo("gradle-aem-plugin")
+    token((project.findProperty("github.token") ?: "").toString())
+    tagName(project.version.toString())
+    releaseName(project.version.toString())
+    releaseAssets(project.fileTree("build/libs") { include("**/${project.name}-${project.version}*.jar") })
+    draft((project.findProperty("github.draft") ?: "false").toString().toBoolean())
+    prerelease((project.findProperty("github.prerelease") ?: "false").toString().toBoolean())
+    overwrite((project.findProperty("github.override") ?: "false").toString().toBoolean())
+
+    body { """
+    |# What's new
+    |
+    |TBD
+    |
+    |# Upgrade notes
+    |
+    |Nothing to do.
+    |
+    |# Contributions
+    |
+    |None.
+    |
+    |# Change log
+    |
+    |${changelog().call()}
+    """.trimMargin()
+    }
+}
+
 apply(from = "gradle/fork.gradle.kts")
