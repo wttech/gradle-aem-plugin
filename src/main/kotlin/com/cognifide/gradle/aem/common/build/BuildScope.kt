a32a405469ad40f346201c7693edebe7164cdda8
krystian.panek <krystian.panek@cognifide.com>
2019-11-13 16:42:26 +0100

    Minor

--- src/main/kotlin/com/cognifide/gradle/aem/common/build/BuildScope.kt
+++ src/main/kotlin/com/cognifide/gradle/aem/common/build/BuildScope.kt
@@ -22,6 +22,7 @@ class BuildScope {
         }
     }
 
+    @Synchronized
     fun <T : Any> tryGetOrPut(key: String, defaultValue: () -> T?): T? {
         var result: T? = get(key)
         if (result == null) {
@@ -38,6 +39,7 @@ class BuildScope {
         cache[key] = value
     }
 
+    @Synchronized // TODO bad synchronization ; sh gradlew packageCompose (when deleted sync.cnd at multi)
     fun doOnce(operation: String, action: () -> Unit) = tryGetOrPut(operation) {
         action()
         true
