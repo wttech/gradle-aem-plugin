1a491b08c92111bd1b201389b9b9b5d9902238dc
mierzwid <mierzwid@gmail.com>
2020-01-30 15:49:39 +0100

    sample project for AEM on Java 11 blog post

--- docs/blog/2020-02-aem-on-java-11/build.gradle.kts
+++ docs/blog/2020-02-aem-on-java-11/build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     id("com.cognifide.aem.instance")
 }
 
+repositories {
+    jcenter()
+}
+
 aem {
     tasks {
         instanceProvision {
@@ -21,7 +25,7 @@ aem {
         instanceSatisfy {
             packages {
                 "dep.acs-aem-commons"("https://github.com/Adobe-Consulting-Services/acs-aem-commons/releases/download/acs-aem-commons-4.0.0/acs-aem-commons-content-4.0.0-min.zip")
-                "dep.kotlin"("org.jetbrains.kotlin:kotlin-osgi-bundle:1.3.50")
+                "dep.kotlin"("org.jetbrains.kotlin:kotlin-osgi-bundle:1.3.61")
                 "dep.groovy-console"("https://github.com/icfnext/aem-groovy-console/releases/download/13.0.0/aem-groovy-console-13.0.0.zip")
             }
         }
--- docs/blog/2020-02-aem-on-java-11/buildSrc/build.gradle.kts
+++ docs/blog/2020-02-aem-on-java-11/buildSrc/build.gradle.kts
@@ -1,8 +1,10 @@
 repositories {
+    mavenLocal()
+    jcenter()
     gradlePluginPortal()
 }
 
 dependencies {
-    implementation("com.cognifide.gradle:aem-plugin:10.1.3")
+    implementation("com.cognifide.gradle:aem-plugin:10.2.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
 }
