f50a0083e705f029e9820319feb50a93cc9100eb
mierzwid <mierzwid@gmail.com>
2020-01-31 10:47:38 +0100

    sample project for AEM on Java 11 blog post - fixing plugin version

--- docs/blog/2020-02-aem-on-java-11/buildSrc/build.gradle.kts
+++ docs/blog/2020-02-aem-on-java-11/buildSrc/build.gradle.kts
@@ -1,10 +1,8 @@
 repositories {
-    mavenLocal()
-    jcenter()
     gradlePluginPortal()
 }
 
 dependencies {
-    implementation("com.cognifide.gradle:aem-plugin:10.2.0")
+    implementation("com.cognifide.gradle:aem-plugin:10.1.5")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
 }
