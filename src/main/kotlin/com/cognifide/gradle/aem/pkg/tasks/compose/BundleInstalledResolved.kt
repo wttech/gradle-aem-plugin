48941347bdb2b134ddda3e390aac8f3debd1497f
Krystian Panek <krystian.panek@cognifide.com>
2020-02-20 22:32:13 +0100

    Package test fixed after refactoring

--- src/main/kotlin/com/cognifide/gradle/aem/pkg/tasks/compose/BundleInstalledResolved.kt
+++ src/main/kotlin/com/cognifide/gradle/aem/pkg/tasks/compose/BundleInstalledResolved.kt
@@ -1,7 +1,7 @@
 package com.cognifide.gradle.aem.pkg.tasks.compose
 
 import com.cognifide.gradle.aem.pkg.tasks.PackageCompose
-import com.cognifide.gradle.common.build.DependencyOptions
+import com.cognifide.gradle.common.build.DependencyFile
 import org.gradle.api.tasks.Input
 
 class BundleInstalledResolved(private val target: PackageCompose, @Input val notation: Any) : BundleInstalled {
@@ -10,11 +10,13 @@ class BundleInstalledResolved(private val target: PackageCompose, @Input val not
 
     private val project = aem.project
 
-    override val file = aem.obj.file { fileProvider(aem.obj.provider { DependencyOptions.resolveFile(project, notation) }) }
+    private val dependencyFile = DependencyFile(project, notation)
+
+    override val file = aem.obj.file { fileProvider(aem.obj.provider { dependencyFile.file }) }
 
     override val dirPath = aem.obj.string { convention(target.bundlePath) }
 
-    override val fileName = aem.obj.string { convention(aem.obj.provider { DependencyOptions.determineFileName(project, notation) }) }
+    override val fileName = aem.obj.string { convention(aem.obj.provider { dependencyFile.file.name }) }
 
     override val vaultFilter = aem.obj.boolean { convention(target.vaultFilters) }
 }
--- src/main/kotlin/com/cognifide/gradle/aem/pkg/tasks/compose/PackageNestedResolved.kt
+++ src/main/kotlin/com/cognifide/gradle/aem/pkg/tasks/compose/PackageNestedResolved.kt
@@ -1,7 +1,8 @@
 package com.cognifide.gradle.aem.pkg.tasks.compose
 
+import com.cognifide.gradle.aem.common.pkg.PackageFile
 import com.cognifide.gradle.aem.pkg.tasks.PackageCompose
-import com.cognifide.gradle.common.build.DependencyOptions
+import com.cognifide.gradle.common.build.DependencyFile
 import org.gradle.api.tasks.Input
 
 class PackageNestedResolved(private val target: PackageCompose, @Input val notation: Any) : PackageNested {
@@ -10,19 +11,13 @@ class PackageNestedResolved(private val target: PackageCompose, @Input val notat
 
     private val project = aem.project
 
-    override val file = aem.obj.file { fileProvider(aem.obj.provider {
-        DependencyOptions.resolveFile(project, DependencyOptions.hintNotation(notation, NOTATION_EXTENSION)) })
-    }
+    private val dependencyFile = DependencyFile(project, DependencyFile.hintNotation(notation, "zip"))
 
-    override val dirPath = aem.obj.string { convention(target.nestedPath) } // TODO append group extracted from notation
+    override val file = aem.obj.file { fileProvider(aem.obj.provider { dependencyFile.file }) }
 
-    override val fileName = aem.obj.string { convention(aem.obj.provider {
-        DependencyOptions.determineFileName(project, DependencyOptions.hintNotation(notation, NOTATION_EXTENSION)) }) // TODO impl determineFileName
-    }
+    override val dirPath = aem.obj.string { convention(target.nestedPath.map { "$it/${PackageFile(dependencyFile.file).group}" }) }
+
+    override val fileName = aem.obj.string { convention(aem.obj.provider { dependencyFile.file.name }) }
 
     override val vaultFilter = aem.obj.boolean { convention(target.vaultFilters) }
-
-    companion object {
-        const val NOTATION_EXTENSION = "zip"
-    }
 }
