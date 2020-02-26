d0565a32e0f11f4114ca3d315ec7add85607106a
Krystian Panek <krystian.panek@cognifide.com>
2020-02-26 10:19:11 +0100

    Fix

--- src/main/kotlin/com/cognifide/gradle/aem/common/pkg/vault/CndSync.kt
+++ src/main/kotlin/com/cognifide/gradle/aem/common/pkg/vault/CndSync.kt
@@ -10,21 +10,29 @@ class CndSync(private val aem: AemExtension) {
 
     val file = aem.obj.file()
 
-    val type = aem.obj.typed<CndSyncType> { convention(CndSyncType.PRESERVE) }
+    val type = aem.obj.typed<CndSyncType> {
+        convention(aem.obj.provider {
+            when {
+                aem.commonOptions.offline.get() -> CndSyncType.NEVER
+                else -> CndSyncType.PRESERVE
+            }
+        })
+    }
 
     fun type(name: String) {
         type.set(CndSyncType.of(name))
     }
 
     fun sync() {
-        val file = file.get().asFile
-        val unavailableMessage by lazy { "Cannot synchronize CND file '$file' because none of AEM instances are available!" }
-
+        val file = file.orNull?.asFile
+                ?: throw PackageException("CND file to be synchronized is not specified!")
         when (type.get()) {
-            CndSyncType.ALWAYS -> syncOrElse { throw PackageException(unavailableMessage) }
+            CndSyncType.ALWAYS -> syncOrElse {
+                throw PackageException("CND file '$file' cannot be synchronized as of none of AEM instances are available!")
+            }
             CndSyncType.PRESERVE -> {
-                if (!file.exists()) {
-                    syncOrElse { aem.logger.warn(unavailableMessage) }
+                if (!file.exists()) syncOrElse {
+                    aem.logger.warn("CND file '$file' does not exist and moreover cannot be synchronized as of none of AEM instances are available!")
                 }
             }
             CndSyncType.NEVER -> {}
