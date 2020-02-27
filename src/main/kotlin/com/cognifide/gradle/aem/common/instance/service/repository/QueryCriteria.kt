40585dadd640a9e3d57d4bd24c87f1733eab634b
Krystian Panek <krystian.panek@cognifide.com>
2020-02-27 14:42:30 +0100

    Repo query or/and operators

--- src/main/kotlin/com/cognifide/gradle/aem/common/instance/service/repository/QueryCriteria.kt
+++ src/main/kotlin/com/cognifide/gradle/aem/common/instance/service/repository/QueryCriteria.kt
@@ -1,143 +1,26 @@
 package com.cognifide.gradle.aem.common.instance.service.repository
 
 @Suppress("TooManyFunctions")
-class QueryCriteria {
+class QueryCriteria : QueryParams(false) {
 
-    // All params
+    // Grouped params (logic statements)
 
-    val params = mutableMapOf<String, String>()
+    fun or(paramsDefiner: QueryParams.() -> Unit) = group(true, paramsDefiner)
 
-    // Custom params
+    fun and(paramsDefiner: QueryParams.() -> Unit) = group(false, paramsDefiner)
 
-    fun property(definition: (Int) -> Unit) {
-        definition(propertyIndex)
+    private fun group(or: Boolean, paramsDefiner: QueryParams.() -> Unit) {
+        val params = QueryParams(true).apply(paramsDefiner).params
+        val index = groupIndex
+
+        this.params["${index}_group.p.or"] = or.toString()
+        this.params.putAll(params.mapKeys { "${index}_group.${it.key}" })
     }
 
-    private val propertyIndex: Int get() = (params.keys
-            .filter { it.endsWith("_property") }
+    private val groupIndex: Int get() = (params.keys
+            .filter { it.matches(Regex("^\\d+_group\\..*")) }
             .map { it.split("_")[0].toInt() }
             .max() ?: 0) + 1
-
-    fun group(name: String, values: Iterable<String>, or: Boolean = true) {
-        params["group.p.or"] = or.toString()
-        values.forEachIndexed { vi, value ->
-            params["group.${vi + 1}_$name"] = value
-        }
-    }
-
-    // Node type filtering params
-
-    fun type(value: String) {
-        params["type"] = value
-    }
-
-    fun types(vararg values: String) = types(values.asIterable())
-
-    fun types(values: Iterable<String>) {
-        values.toList().apply {
-            when (size) {
-                1 -> type(first())
-                else -> group("type", values)
-            }
-        }
-    }
-
-    fun file() = type("nt:file")
-
-    fun page() = type("cq:Page")
-
-    fun pageContent() = type("cq:PageContent")
-
-    fun damAsset() = type("dam:Asset")
-
-    // Specialized filtering params
-
-    fun name(wildcardPattern: String) {
-        params["nodename"] = wildcardPattern
-    }
-
-    fun path(value: String) {
-        params["path"] = value
-    }
-
-    fun paths(vararg values: String) = paths(values.asIterable())
-
-    fun paths(values: Iterable<String>) {
-        values.toList().apply {
-            when (size) {
-                1 -> path(first())
-                else -> group("path", values)
-            }
-        }
-    }
-
-    fun depth(value: Int) {
-        params["p.nodedepth"] = value.toString()
-    }
-
-    fun fullText(value: String, relativePath: String? = null) {
-        params["fulltext"] = value
-        if (relativePath != null) {
-            params["fulltext.relPath"] = relativePath
-        }
-    }
-
-    fun tag(value: String, property: String? = null) {
-        params["tagid"] = value
-        if (property != null) {
-            params["tagid.property"] = property
-        }
-    }
-
-    fun contentTag(value: String) = tag(value, "jcr:content/cq:tags")
-
-    // Custom property filtering params
-
-    fun propertyWhere(name: String, value: String? = null, operation: String? = null) = property { p ->
-        params["${p}_property"] = name
-        if (value != null) {
-            params["${p}_property.value"] = value
-        }
-        if (operation != null) {
-            params["${p}_property.operation"] = operation
-        }
-    }
-
-    fun property(name: String, value: String) = propertyWhere(name, value)
-
-    fun propertyLike(name: String, value: String) = propertyWhere(name, value, "like")
-
-    fun propertyExists(name: String) = propertyWhere(name, true.toString(), "exists")
-
-    fun propertyNotExists(name: String) = propertyWhere(name, false.toString(), "exists")
-
-    fun propertyNot(name: String) = propertyWhere(name, null, "not")
-
-    fun propertyEquals(name: String, value: String, ignoreCase: Boolean = false) = when (ignoreCase) {
-        true -> propertyWhere(name, value, "equalsIgnoreCase")
-        false -> propertyWhere(name, value, "equals")
-    }
-
-    fun propertyNotEquals(name: String, value: String, ignoreCase: Boolean = false) = when (ignoreCase) {
-        true -> propertyWhere(name, value, "unequalsIgnoreCase")
-        false -> propertyWhere(name, value, "unequals")
-    }
-
-    fun propertyContains(name: String, values: Iterable<String>, all: Boolean = true) = property { pi ->
-        params["${pi}_property"] = name
-        if (all) {
-            params["${pi}_property.and"] = "true"
-        }
-        values.forEachIndexed { vi, value ->
-            params["${pi}_property.${vi + 1}_value"] = value
-        }
-    }
-
-    fun propertyContains(name: String, vararg values: String, all: Boolean = true) = propertyContains(name, values.asIterable(), all)
-
-    fun propertyContainsAny(name: String, values: Iterable<String>) = propertyContains(name, values, false)
-
-    fun propertyContainsAny(name: String, vararg values: String) = propertyContains(name, values.asIterable(), false)
 
     // Ordering params
 
