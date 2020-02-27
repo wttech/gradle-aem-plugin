2ecaf1a639197ac4bad34519e15ee7b2272ebcbc
mierzwid <mierzwid@gmail.com>
2020-02-27 09:17:49 +0100

    renaming tests

--- src/functionalTest/kotlin/com/cognifide/gradle/aem/instance/local/LocalInstancePluginTest.kt
+++ src/functionalTest/kotlin/com/cognifide/gradle/aem/instance/local/LocalInstancePluginTest.kt
@@ -5,7 +5,7 @@ import org.gradle.internal.impldep.org.testng.AssertJUnit.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.condition.EnabledIfSystemProperty
 
-class SetupAndBackupTest : AemBuildTest() {
+class LocalInstancePluginTest : AemBuildTest() {
 
     @Test
     fun `should apply plugin correctly`() {
@@ -23,7 +23,6 @@ class SetupAndBackupTest : AemBuildTest() {
             assertTask(":tasks")
         }
     }
-
 
     @EnabledIfSystemProperty(named = "localInstance.jarUrl", matches = ".+")
     @Test
@@ -157,6 +156,55 @@ class SetupAndBackupTest : AemBuildTest() {
 
         runBuild(projectDir, "assertIfCrxDeEnabled") {
             assertTask(":assertIfCrxDeEnabled")
+        }
+
+        runBuild(projectDir, "instanceDestroy", "-Pforce") {
+            assertTask(":instanceDestroy")
+        }
+
+        runBuild(projectDir, "clean") {
+            assertTask(":clean")
+        }
+    }
+
+    @EnabledIfSystemProperty(named = "localInstance.jarUrl", matches = ".+")
+    @Test
+    fun `should re-setup local aem author and publish instances`() {
+        val projectDir = prepareProject("local-instance-resetup") {
+            gradleProperties("""
+                fileTransfer.user=${System.getProperty("fileTransfer.user")}
+                fileTransfer.password=${System.getProperty("fileTransfer.password")}
+                fileTransfer.domain=${System.getProperty("fileTransfer.domain")}
+                
+                localInstance.quickstart.jarUrl=${System.getProperty("localInstance.jarUrl")}
+                localInstance.quickstart.licenseUrl=${System.getProperty("localInstance.licenseUrl")}
+                
+                instance.local-author.httpUrl=http://localhost:9502
+                instance.local-author.type=local
+                instance.local-author.runModes=local,nosamplecontent
+                instance.local-author.jvmOpts=-server -Xmx2048m -XX:MaxPermSize=512M -Djava.awt.headless=true
+                """)
+
+            settingsGradle("")
+
+            buildGradle("""
+                plugins {
+                    id("com.cognifide.aem.instance.local")
+                }
+                """)
+        }
+
+        runBuild(projectDir, "instanceResetup", "-Pforce") {
+            assertTask(":instanceDown")
+            assertTask(":instanceDestroy")
+            assertTask(":instanceCreate")
+            assertTask(":instanceUp")
+            assertTask(":instanceSatisfy")
+            assertTask(":instanceProvision")
+            assertTask(":instanceSetup")
+            assertTask(":instanceResetup")
+            assertFileExists(".instance/author")
+            assertFileNotExists(".instance/publish")
         }
 
         runBuild(projectDir, "instanceDestroy", "-Pforce") {
