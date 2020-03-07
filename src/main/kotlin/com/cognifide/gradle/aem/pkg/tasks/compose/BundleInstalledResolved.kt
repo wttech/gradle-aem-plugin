6eabe6d237dfac0917b256a152b8be2b79534e86
Krystian Panek <krystian.panek@cognifide.com>
2020-03-07 21:13:48 +0100

    Package resolved and bundle resolved in package compose (from urls)

--- src/main/kotlin/com/cognifide/gradle/aem/pkg/tasks/compose/BundleInstalledResolved.kt
+++ src/main/kotlin/com/cognifide/gradle/aem/pkg/tasks/compose/BundleInstalledResolved.kt
@@ -1,22 +1,19 @@
 package com.cognifide.gradle.aem.pkg.tasks.compose
 
 import com.cognifide.gradle.aem.pkg.tasks.PackageCompose
-import com.cognifide.gradle.common.build.DependencyFile
 import org.gradle.api.tasks.Input
 
 class BundleInstalledResolved(private val target: PackageCompose, @Input val notation: Any) : BundleInstalled {
 
     private val aem = target.aem
 
-    private val project = aem.project
+    private val resolvedFile by lazy { aem.common.resolveFile(notation) }
 
-    private val dependencyFile = DependencyFile(project, notation)
-
-    override val file = aem.obj.file { fileProvider(aem.obj.provider { dependencyFile.file }) }
+    override val file = aem.obj.file { fileProvider(aem.obj.provider { resolvedFile }) }
 
     override val dirPath = aem.obj.string { convention(target.bundlePath) }
 
-    override val fileName = aem.obj.string { convention(aem.obj.provider { dependencyFile.file.name }) }
+    override val fileName = aem.obj.string { convention(aem.obj.provider { resolvedFile.name }) }
 
     override val vaultFilter = aem.obj.boolean { convention(target.vaultFilters) }
 
