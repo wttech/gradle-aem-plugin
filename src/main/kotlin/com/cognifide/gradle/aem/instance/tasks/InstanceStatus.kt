aee03e90d0602e3cf6581579d9d57092c43a3266
Krystian Panek <krystian.panek@cognifide.com>
2020-03-31 21:32:35 +0200

    Instance status could check packages coming from any source

--- src/main/kotlin/com/cognifide/gradle/aem/instance/tasks/InstanceStatus.kt
+++ src/main/kotlin/com/cognifide/gradle/aem/instance/tasks/InstanceStatus.kt
@@ -1,7 +1,9 @@
 package com.cognifide.gradle.aem.instance.tasks
 
+import com.cognifide.gradle.aem.common.instance.Instance
 import com.cognifide.gradle.aem.common.instance.LocalInstance
 import com.cognifide.gradle.aem.common.tasks.InstanceTask
+import com.cognifide.gradle.aem.pkg.tasks.PackageCompose
 import com.cognifide.gradle.common.utils.Formats
 import com.cognifide.gradle.common.utils.onEachApply
 import de.vandermeer.asciitable.AsciiTable
@@ -16,6 +18,14 @@ open class InstanceStatus : InstanceTask() {
         convention(logger.isInfoEnabled)
         aem.prop.boolean("instance.status.detailed")?.let { set(it) }
     }
+
+    @Internal
+    val packagesBuilt = aem.obj.list<PackageCompose> {
+        convention(aem.obj.provider { aem.packagesBuilt })
+    }
+
+    @Internal
+    val packages = aem.obj.files()
 
     @TaskAction
     @Suppress("MagicNumber", "LongMethod", "TooGenericExceptionCaught")
@@ -34,50 +44,7 @@ open class InstanceStatus : InstanceTask() {
                 } else {
                     instances.get().onEachApply {
                         increment("Checking status of instance '$name'") {
-                            val instanceDetails = mutableListOf<String>().apply {
-                                add("URL: $httpUrl | Available: $available")
-                                add("Name: $name | Version: $version")
-
-                                if (this@onEachApply is LocalInstance) {
-                                    add("Status: ${status.displayName} | Debug port: $debugPort")
-                                }
-
-                                if (available) {
-                                    add("State check: $state")
-                                    add("Run path: $runningPath")
-                                    add("Run modes: ${runningModes.joinToString(",")}")
-
-                                    if (detailed.get()) {
-                                        add("Time zone: ${zoneId.id} (GMT${zoneOffset.id})")
-                                        add("Operating system: $osInfo")
-                                        add("Java: $javaInfo")
-                                    }
-                                }
-                            }.joinToString("<br>")
-
-                            val packagesInstalled = if (available) {
-                                try {
-                                    sync {
-                                        packageManager.listRetry.never()
-                                        aem.packagesBuilt.sortedBy { it.path }.mapNotNull { task ->
-                                            val pkg = packageManager.find(task.vaultDefinition)
-                                            val path = task.path.removeSuffix(":${task.name}")
-
-                                            if (pkg != null && pkg.installed) {
-                                                "$path (${Formats.date(date(pkg.lastUnpacked!!))})"
-                                            } else {
-                                                "$path (not yet)"
-                                            }
-                                        }.joinToString("<br>")
-                                    }.ifBlank { "none" }
-                                } catch (e: Exception) {
-                                    "unknown"
-                                }
-                            } else {
-                                "unknown"
-                            }
-
-                            addRow(instanceDetails, packagesInstalled)
+                            addRow(instanceDetails(), packagesInstalled())
                             addRule()
                         }
                     }
@@ -88,6 +55,59 @@ open class InstanceStatus : InstanceTask() {
         }
 
         println(table.render())
+    }
+
+    private fun Instance.instanceDetails() = mutableListOf<String>().apply {
+        add("URL: $httpUrl | Available: $available")
+        add("Name: $name | Version: $version")
+
+        if (this@instanceDetails is LocalInstance) {
+            add("Status: ${status.displayName} | Debug port: $debugPort")
+        }
+
+        if (available) {
+            add("State check: $state")
+            add("Run path: $runningPath")
+            add("Run modes: ${runningModes.joinToString(",")}")
+
+            if (detailed.get()) {
+                add("Time zone: ${zoneId.id} (GMT${zoneOffset.id})")
+                add("Operating system: $osInfo")
+                add("Java: $javaInfo")
+            }
+        }
+    }.joinToString("<br>")
+
+    private fun Instance.packagesInstalled() = if (available) {
+        try {
+            sync {
+                packageManager.listRetry.never()
+                packagesBuilt.get().sortedBy { it.path }.mapNotNull { task ->
+                    val pkg = packageManager.find(task.vaultDefinition)
+                    val path = task.path.removeSuffix(":${task.name}")
+
+                    if (pkg != null && pkg.installed) {
+                        "$path (${Formats.date(date(pkg.lastUnpacked!!))})"
+                    } else {
+                        "$path (not yet)"
+                    }
+                }.joinToString("<br>")
+                packages.files.sortedBy { it.name }.mapNotNull { file ->
+                    val pkg = packageManager.find(file)
+                    val name = file.name
+
+                    if (pkg != null && pkg.installed) {
+                        "$name (${Formats.date(date(pkg.lastUnpacked!!))})"
+                    } else {
+                        "$name (not yet)"
+                    }
+                }.joinToString("<br>")
+            }.ifBlank { "none" }
+        } catch (e: Exception) {
+            "unknown"
+        }
+    } else {
+        "unknown"
     }
 
     init {
