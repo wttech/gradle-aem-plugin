9562a06d2fb1bb2d201a5e45e6e6c70df4ba388b
Krystian Panek <krystian.panek@cognifide.com>
2020-04-27 10:03:10 +0200

    Ignore exist status on Windows

--- src/main/kotlin/com/cognifide/gradle/aem/common/utils/WebBrowser.kt
+++ src/main/kotlin/com/cognifide/gradle/aem/common/utils/WebBrowser.kt
@@ -22,6 +22,7 @@ class WebBrowser(private val aem: AemExtension) {
             ProcBuilder(command, url)
                     .withWorkingDirectory(aem.project.projectDir)
                     .withTimeoutMillis(TimeUnit.SECONDS.toMillis(30))
+                    .ignoreExitStatus()
                     .withInputStream(SafeStreams.emptyInput())
                     .withOutputStream(SafeStreams.systemOut())
                     .withErrorStream(SafeStreams.systemOut())
