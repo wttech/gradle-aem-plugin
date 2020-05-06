a4eb257ff6941eb9d17b97266f5d03a917ac4712
Krystian Panek <krystian.panek@cognifide.com>
2020-05-06 21:20:46 +0200

    Approach to be changed

--- launcher/src/main/kotlin/com/cognifide/gradle/aem/launcher/Launcher.kt
+++ launcher/src/main/kotlin/com/cognifide/gradle/aem/launcher/Launcher.kt
@@ -2,8 +2,10 @@ package com.cognifide.gradle.aem.launcher
 
 import org.gradle.tooling.GradleConnector
 import java.io.File
+import kotlin.system.exitProcess
 
 class Launcher {
+
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
@@ -21,33 +23,45 @@ class Launcher {
                     }
                 }
 
+                resolve("buildSrc/build.gradle.kts").apply {
+                    parentFile.mkdirs()
+
+                    writeText("""
+                        dependencies {
+                            implementation(files("../libs/gap.jar"))
+                        }
+                    """.trimIndent())
+                }
+
+                resolve("settings.gradle.kts").writeText("""
+                    rootProject.name = "gap"
+                """.trimIndent())
+
                 resolve("build.gradle.kts").writeText("""
-                    buildscript {
-                        dependencies.classpath(fileTree("libs"))
-                    }
-                    
                     plugins {
                         id("com.cognifide.aem.instance.local")
                         id("com.cognifide.aem.package.sync")
                     }
                 """.trimIndent())
-
-                resolve("settings.gradle.kts").writeText("""
-                    rootProject.name = "gap"
-                """.trimIndent())
             }
 
-            GradleConnector.newConnector()
-                    .forProjectDirectory(workDir)
-                    .connect().use { connection ->
-                        connection.newBuild()
-                                .withArguments(args.asIterable())
-                                .setColorOutput(true)
-                                .setStandardOutput(System.out)
-                                .setStandardError(System.err)
-                                .setStandardInput(System.`in`)
-                                .run()
-                    }
+            try {
+                GradleConnector.newConnector()
+                        .forProjectDirectory(workDir)
+                        .connect().use { connection ->
+                            connection.newBuild()
+                                    .withArguments(args.asIterable())
+                                    .setColorOutput(true)
+                                    .setStandardOutput(System.out)
+                                    .setStandardError(System.err)
+                                    .setStandardInput(System.`in`)
+                                    .run()
+                        }
+                exitProcess(0)
+            } catch (e: Exception) {
+                e.printStackTrace(System.err)
+                exitProcess(1)
+            }
         }
     }
 }
