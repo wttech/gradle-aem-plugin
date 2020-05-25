3b923eba7a6a9317e75bae268fd2d7b4df27d9e3
Krystian Panek <krystian.panek@cognifide.com>
2020-05-25 23:46:43 +0200

    Satisfy deploy package fix

--- src/test/kotlin/com/cognifide/gradle/aem/common/instance/provision/step/DeployPackageSourceTest.kt
+++ src/test/kotlin/com/cognifide/gradle/aem/common/instance/provision/step/DeployPackageSourceTest.kt
@@ -12,46 +12,46 @@ class DeployPackageSourceTest {
         // Real use cases
         assertEquals(
                 DeployPackageSource("search-webconsole-plugin", "1.3.0"),
-                DeployPackageSource.from("https://github.com/neva-dev/felix-search-webconsole-plugin" +
+                DeployPackageSource.fromUrlOrNotation("https://github.com/neva-dev/felix-search-webconsole-plugin" +
                         "/releases/download/search-webconsole-plugin-1.3.0/search-webconsole-plugin-1.3.0.jar")
         )
         assertEquals(
                 DeployPackageSource("core.wcm.components.all", "2.8.0"),
-                DeployPackageSource.from("com.adobe.cq:core.wcm.components.all:2.8.0@zip")
+                DeployPackageSource.fromUrlOrNotation("com.adobe.cq:core.wcm.components.all:2.8.0@zip")
         )
         assertEquals(
                 DeployPackageSource("search-webconsole-plugin", "1.3.0"),
-                DeployPackageSource.from("com.neva.felix:search-webconsole-plugin:1.3.0")
+                DeployPackageSource.fromUrlOrNotation("com.neva.felix:search-webconsole-plugin:1.3.0")
         )
 
         // Fake / potential edge cases
         assertThrows<ProvisionException> {
-            DeployPackageSource.from("https://test.com/packages/neva-dev/felix-search-webconsole-1.0")
+            DeployPackageSource.fromUrlOrNotation("https://test.com/packages/neva-dev/felix-search-webconsole-1.0")
         }
 
         assertEquals(
                 DeployPackageSource("felix-search-webconsole", "1.0"),
-                DeployPackageSource.from("https://test.com/some-dir/felix-search-webconsole-1.0.zip")
+                DeployPackageSource.fromUrlOrNotation("https://test.com/some-dir/felix-search-webconsole-1.0.zip")
         )
         assertEquals(
                 DeployPackageSource("felix-search-webconsole", "1.0"),
-                DeployPackageSource.from("https://test.com/some-dir/felix-search-webconsole-1.0.jar")
+                DeployPackageSource.fromUrlOrNotation("https://test.com/some-dir/felix-search-webconsole-1.0.jar")
         )
         assertEquals(
                 DeployPackageSource("felix.search.webconsole", "1.0.ALPHA"),
-                DeployPackageSource.from("https://test.com/aa/bb/cc/felix.search.webconsole-1.0.ALPHA.zip")
+                DeployPackageSource.fromUrlOrNotation("https://test.com/aa/bb/cc/felix.search.webconsole-1.0.ALPHA.zip")
         )
         assertEquals(
                 DeployPackageSource("felix-search-webconsole", "1.1.23"),
-                DeployPackageSource.from("https://test.com/some-dir/felix-search-webconsole-1.1.23.zip")
+                DeployPackageSource.fromUrlOrNotation("https://test.com/some-dir/felix-search-webconsole-1.1.23.zip")
         )
         assertEquals(
                 DeployPackageSource("felix-search-webconsole", "1.0.0.RELEASE"),
-                DeployPackageSource.from("https://test.com/some-dir/felix-search-webconsole-1.0.0.RELEASE.zip")
+                DeployPackageSource.fromUrlOrNotation("https://test.com/some-dir/felix-search-webconsole-1.0.0.RELEASE.zip")
         )
         assertEquals(
                 DeployPackageSource("felix-search-webconsole", "1.0.0-SNAPSHOT"),
-                DeployPackageSource.from("https://test.com/felix-search-webconsole-1.0.0-SNAPSHOT.zip")
+                DeployPackageSource.fromUrlOrNotation("https://test.com/felix-search-webconsole-1.0.0-SNAPSHOT.zip")
         )
     }
 }
