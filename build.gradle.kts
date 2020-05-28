5c9cbc6d445e72428d1d99aca0074fdeb140367c
Krystian Panek <krystian.panek@cognifide.com>
2020-05-28 10:11:30 +0200

    GAP 14 release

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ repositories {
 dependencies {
     implementation(gradleApi())
 
-    implementation("com.cognifide.gradle:common-plugin:0.1.52")
+    implementation("com.cognifide.gradle:common-plugin:0.1.53")
 
     implementation("org.jsoup:jsoup:1.12.1")
     implementation("org.buildobjects:jproc:2.3.0")
