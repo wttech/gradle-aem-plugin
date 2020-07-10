c31866912b9c78449e87be4d4360131127a0ef26
mierzwid <mierzwid@gmail.com>
2020-07-10 18:28:12 +0200

    [BLOG] AEM Dispatcher with ease - project skeleton

---
+++ docs/blog/2020-08-01-dispatcher/buildSrc/build.gradle.kts
@@ -0,0 +1,8 @@
+repositories {
+    gradlePluginPortal()
+}
+
+dependencies {
+    implementation("com.cognifide.gradle:aem-plugin:14.1.0")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
+}
---
+++ docs/blog/2020-08-01-dispatcher/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "aem-dispatcher-with-ease"
