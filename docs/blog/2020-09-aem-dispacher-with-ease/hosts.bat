ad964595728b80fd2eac4f7c9d8021867451af50
mierzwid <mierzwid@gmail.com>
2020-09-03 15:14:03 +0200

    dispatcher blog post - fixed hosts.bat

--- docs/blog/2020-09-aem-dispacher-with-ease/hosts.bat
+++ docs/blog/2020-09-aem-dispacher-with-ease/hosts.bat
@@ -1,7 +1,1 @@
-del .gap.hosts.tmp
-
-echo gradlew.bat environmentHosts -q > .gap.hosts.tmp
-
-powershell -command "Start-Process cmd -ArgumentList '/C cd %CD% && type .gap.hosts.tmp >> C:\Windows\System32\drivers\etc\hosts' -Verb runas"
-
-del .gap.hosts.tmp
+gradlew.bat environmentHosts -q > .gap.hosts.tmp & powershell -command "Start-Process cmd -ArgumentList '/C cd %CD% && type .gap.hosts.tmp >> C:\Windows\System32\drivers\etc\hosts' -Verb runas"
