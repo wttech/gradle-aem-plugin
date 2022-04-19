9d58643efe83a7b6e7424c4e642db8920e9765ea
mariusz-pacyga-wttech <mariusz.pacyga@wundermanthompson.com>
2022-04-19 16:31:13 +0200

    #865 - added progress bar - added target instances (default: all instances) - fixes

--- src/main/kotlin/com/cognifide/gradle/aem/instance/tasks/InstanceWorkflow.kt
+++ src/main/kotlin/com/cognifide/gradle/aem/instance/tasks/InstanceWorkflow.kt
@@ -1,39 +1,68 @@
 package com.cognifide.gradle.aem.instance.tasks
 
+import com.cognifide.gradle.aem.common.instance.InstanceException
 import com.cognifide.gradle.aem.common.instance.names
 import com.cognifide.gradle.aem.common.tasks.Instance
+import org.apache.http.HttpStatus
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskAction
 
 open class InstanceWorkflow : Instance() {
 
     private val notifier = common.notifier
+    private var nodesCount = 0
 
-    @get:Internal
-    val workflow by lazy { common.prop.string("instance.workflow") ?: error("No workflow defined!") }
+    @Internal
+    val workflow = common.prop.string("instance.workflow")
 
-    @get:Internal
-    val workflowPath by lazy { common.prop.string("instance.workflow.path") ?: error("No worflow path defined!") }
+    @Internal
+    val workflowPath = common.prop.string("instance.workflow.path")
+
+    @Internal
+    val targetInstaces = when {
+        common.prop.string("instance.author") != null -> aem.localInstances
+        common.prop.string("instance.publish") != null -> aem.authorInstances
+        else -> anyInstances
+    }
 
     @TaskAction
-    fun run() = anyInstances.forEach { instanceWorkflows(it) }.let { notifier.notify("All workflows executed correctly!", "Instances: ${anyInstances.names}") }
+    fun run() {
+        instanceManager.examine(targetInstaces)
+        when {
+            workflow == null -> throw InstanceException("No workflow model defined!")
+            workflowPath == null -> throw InstanceException("No path defined!")
+            else -> {
+                targetInstaces.forEach { instanceWorkflows(it) }
+                notifier.notify("All workflows schedled properly!", "Instances: ${targetInstaces.names}")
+            }
+        }
+    }
 
     fun instanceWorkflows(instance: com.cognifide.gradle.aem.common.instance.Instance) {
         instance.sync.repository {
             val nodes = sync.repository.query {
-                path(workflowPath)
+                workflowPath?.let { path(it) }
                 type(NODE_TYPE)
             }.nodeSequence()
 
-            if (nodes.count() == 0) error("No assets avaialable for processing!")
+            if (nodes.count() == 0) throw InstanceException("There are no assets to be processed in workflow at $workflowPath!")
+            nodesCount += nodes.count()
 
-            logger.lifecycle(">>> Instance: ${instance.id}, Assets to be processed:\n${nodes.map { node -> node.path }.joinToString("\n")}")
+            logger.lifecycle("Instance: ${instance.id}, Assets to be processed:\n${nodes.map { node -> node.path }.joinToString("\n")}")
 
             instance.sync.http {
-                nodes.forEach {
-                    val params = mapOf("model" to "$workflow", "payloadType" to "JCR_PATH", "payload" to "${it.path}")
-                    post(WORKFLOW_POST_URI, params) {
-                        if (!it.statusLine.statusCode.equals(HTTP_SUCCESS_CODE)) error("Workflow creating failed for params: $params!")
+                common.progress(nodes.count()) {
+                    nodes.forEach {
+                        val params = mapOf("model" to "$workflow", "payloadType" to "JCR_PATH", "payload" to it.path)
+                        post(WORKFLOW_POST_URI, params) {
+                            if (!it.statusLine.statusCode.equals(HttpStatus.SC_CREATED)) {
+                                throw InstanceException(
+                                    "Workflow creating failed for ${params.get("payload")} and workflow model: $workflow" +
+                                        "\nStatus: ${it.statusLine}!"
+                                )
+                            }
+                        }
+                        increment()
                     }
                 }
             }
@@ -41,7 +70,7 @@ open class InstanceWorkflow : Instance() {
     }
 
     init {
-        description = "Executing given worflow in given path"
+        description = "Executing given workflow on assets under the specified path"
     }
 
     companion object {
@@ -49,6 +78,5 @@ open class InstanceWorkflow : Instance() {
 
         const val WORKFLOW_POST_URI = "/etc/workflow/instances.json"
         const val NODE_TYPE = "dam:Asset"
-        const val HTTP_SUCCESS_CODE = 201
     }
 }
