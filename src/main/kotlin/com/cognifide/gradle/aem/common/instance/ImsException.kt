3fc6c46f8599935125966497219e94ca06627b6c
Jean Khechfe <jean.khechfe@wundermanthompson.com>
2022-07-16 18:08:04 +0200

    #902 Fix detekt formatting error

--- src/main/kotlin/com/cognifide/gradle/aem/common/instance/ImsException.kt
+++ src/main/kotlin/com/cognifide/gradle/aem/common/instance/ImsException.kt
@@ -2,10 +2,9 @@ package com.cognifide.gradle.aem.common.instance
 
 import com.cognifide.gradle.aem.AemException
 
-class ImsException: AemException {
+class ImsException : AemException {
 
     constructor(message: String, cause: Throwable) : super(message, cause)
 
     constructor(message: String) : super(message)
-
 }
