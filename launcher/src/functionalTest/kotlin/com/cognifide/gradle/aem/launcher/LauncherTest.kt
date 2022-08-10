429d88697f6576de5acc472704db8555c7caa677
Jean Khechfe <jean.khechfe@wundermanthompson.com>
2022-08-10 17:32:16 +0200

    #894 Fix launcher functional test

--- launcher/src/functionalTest/kotlin/com/cognifide/gradle/aem/launcher/LauncherTest.kt
+++ launcher/src/functionalTest/kotlin/com/cognifide/gradle/aem/launcher/LauncherTest.kt
@@ -18,7 +18,7 @@ class LauncherTest {
 
     @Test
     fun shouldGenerateCloudEnvProperly() = makeTestProject("cloud-env", "cloud") {
-        launch("env:environmentResolve")
+        launch()
         assertTrue(
             File("build/functionalTest/cloud-env/env/src/environment/httpd/conf.d/variables/default.vars").readText()
                 .contains("COMMERCE_ENDPOINT")
